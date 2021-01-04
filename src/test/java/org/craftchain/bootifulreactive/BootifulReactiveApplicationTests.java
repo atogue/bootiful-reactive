75a77096d1094df30c3ee4d8ed8a7f0540a0a4c4
Anicet Togue <atogue@gmail.com>
2021-01-04 18:26:44 +0100

    default  springBoot testing contextLoads

---
+++ src/test/java/org/craftchain/bootifulreactive/BootifulReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package org.craftchain.bootifulreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootifulReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
