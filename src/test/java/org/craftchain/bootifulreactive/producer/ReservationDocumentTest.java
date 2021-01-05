219f6d21841bd8f81b90edc81b265ac17870225c
Anicet Togue <atogue@gmail.com>
2021-01-05 18:54:04 +0100

    disable test persistency until a mongodb instance should be running.

--- src/test/java/org/craftchain/bootifulreactive/producer/ReservationDocumentTest.java
+++ src/test/java/org/craftchain/bootifulreactive/producer/ReservationDocumentTest.java
@@ -1,5 +1,6 @@
 package org.craftchain.bootifulreactive.producer;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+@Disabled("disable until a mongodb instance is running on k8s or docker!")
 @DataMongoTest
 @ExtendWith(SpringExtension.class)
 public class ReservationDocumentTest {
